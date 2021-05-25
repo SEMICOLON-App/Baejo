07053948ea38ec94545c5a245f34311000c6fb52
minjun8946 <minjun8946@daum.net>
2021-05-25 21:37:27 +0900

    CREATE; dataclass

---
+++ data/src/main/java/com/example/data/local/entity/SearchHistoryEntity.kt
@@ -0,0 +1,15 @@
+package com.example.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class SearchHistoryEnity (
+
+    @PrimaryKey
+    val waybillNumber: String,
+
+    val courierServiceName: String,
+
+    val date : String
+)
---
+++ data/src/main/java/com/example/data/remote/entity/DeliveryProgressDetailData.kt
@@ -0,0 +1,3 @@
+package com.example.data.remote.entity
+
+data class DeliveryProgresses()
